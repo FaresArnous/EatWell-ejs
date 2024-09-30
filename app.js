06a7f91340223ed428ec84e1403a856a31a8c0fd
fares Arnous <Faris.Arnous@hotmail.com>
2024-09-30 14:14:06 +0300

    added views folder to hold the files thar are served to thr browser

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello world!</h1>");
+});
+
+app.get("/restaurants", function (req, res) {
+  res.send("<h1>Hello world!</h1>");
+});
+
+app.listen(3000);
