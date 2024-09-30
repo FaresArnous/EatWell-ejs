d8aecfe5b035bca23e3e325c4892c217b04e17da
fares Arnous <Faris.Arnous@hotmail.com>
2024-09-30 14:42:11 +0300

    changed the file links to serve the html files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -7,7 +8,23 @@ app.get("/", function (req, res) {
 });
 
 app.get("/restaurants", function (req, res) {
-  res.send("<h1>Hello world!</h1>");
+  const restaurantsFilePath = path.join(__dirname, "Views", "restaurants.html");
+  res.sendFile(restaurantsFilePath);
+});
+
+app.get("/recommend", function (req, res) {
+  const recommendFilePath = path.join(__dirname, "Views", "recommend.html");
+  res.sendFile(recommendFilePath);
+});
+
+app.get("/about", function (req, res) {
+  const aboutFilePath = path.join(__dirname, "Views", "about.html");
+  res.sendFile(aboutFilePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const confirmFilePath = path.join(__dirname, "Views", "confirm.html");
+  res.sendFile(confirmFilePath);
 });
 
 app.listen(3000);
