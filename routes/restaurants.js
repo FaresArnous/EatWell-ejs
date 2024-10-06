e635773d602b3c636f0d39b301ba0e189f5269a3
fares Arnous <Faris.Arnous@hotmail.com>
2024-10-06 09:43:50 +0300

    added Query Parameters to sort resturants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,16 +3,39 @@ const express = require("express");
 const uuid = require("uuid");
 
 const resData = require("../util/resturant-data");
+const { restart } = require("nodemon");
 
 const router = express.Router();
 
 router.get("/restaurants", function (req, res) {
   const restaurantId = req.params.id;
+  let order = req.query.order;
+  let nextOrder = "desc";
+
+  if (order !== "asc" && order !== "desc") {
+    order = "asc";
+  }
+
+  if (order === "desc") {
+    nextOrder = "asc";
+  }
 
   const storedResturant = resData.getStoredRes();
+
+  storedResturant.sort(function (resA, resB) {
+    if (
+      (order === "asc" && resA.name > resB.name) ||
+      (order === "desc" && resB.name > resA.name)
+    ) {
+      return 1;
+    }
+    return -1;
+  });
+
   res.render("restaurants", {
     numberOfRestarents: storedResturant.length,
     restaurants: storedResturant,
+    nextOrder,
   });
 });
 //Dynamic Route
